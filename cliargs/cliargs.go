472febbcd7caa4dfca27aad6a7e5feac6b30fae2
Piyush Sonigra <piyush.sonigra@techholding.co>
2025-03-04 16:28:29 +0530

    fix github actions release job and update cli option description

--- cliargs/cliargs.go
+++ cliargs/cliargs.go
@@ -27,7 +27,7 @@ func customUsage() {
 	fmt.Println("  --port <int>           Port number, this will be ignored if from_port and to_port is passed (optional)")
 	fmt.Println("  --from_port <int>      From port number, It should be passed with to_port. Only from_port is not valid argument (optional)")
 	fmt.Println("  --to_port <int>        To port number, It should be passed with from_port. Only to_port is not valid argument (optional)")
-	fmt.Println("  --protocol <int>       Protocol e.g tcp,udp (optional)")
+	fmt.Println("  --protocol <int>       Protocol e.g TCP, UPD, all (optional)")
 	fmt.Println("  --ip <string>          IP address with subnetmask e.g '10.10.19.1/32' (optional)")
 	fmt.Println("  --rule_name <string>   Security group rule name (optional)")
 }
@@ -40,7 +40,7 @@ func ParseArgs() *Args {
 	flag.StringVar(&args.Port, "port", "", "Port number, this will be ignored if from_port and to_port is passed (optional)")
 	flag.StringVar(&args.FromPort, "from_port", "", "Port number, It should be passed with to_port. Only from_port is not valid argument (optional)")
 	flag.StringVar(&args.ToPort, "to_port", "", "Port number, It should be passed with from_port. Only to_port is not valid argument (optional)")
-	flag.StringVar(&args.Protocol, "protocol", "", "Protocol e.g., TCP, UDP (optional)")
+	flag.StringVar(&args.Protocol, "protocol", "", "Protocol e.g., TCP, UDP, all (optional)")
 	flag.StringVar(&args.IP, "ip", "", "IP address with subnetmask e.g., '10.10.19.1/32' (optional)")
 	flag.StringVar(&args.RuleName, "rule_name", "", "Security group rule name (optional)")
 
